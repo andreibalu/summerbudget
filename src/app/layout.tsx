5068af84d179b2d2b0a69a3c871ae06f080aefd1
Andrei Baluta <Andy23589@gmail.com>
2025-06-04 11:41:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <head>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    >
    > ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    >                       <html lang="en">
    > >                       <head>
    >                           <link>
    >                           <link>
    >                           <link>
    >                           <link>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    >                           <meta>
    > >                         {" "}
    >                           ...
    >                         ...
    >                     ...
    >         ...
    > Call Stack
    > 13
    > Show 12 ignore-listed frame(s)
    > head
    > unknown (0:0)
    >

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-        
         <link rel="manifest" href="/manifest.json" />
         <meta name="application-name" content={APP_NAME} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -43,14 +42,12 @@ export default function RootLayout({
         <meta name="description" content={APP_DESCRIPTION} />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#A0D6B4" /> {/* Corresponds to primary color */}
-        
+        <meta name="theme-color" content="#A0D6B4" />{/* Corresponds to primary color */}
         <link rel="apple-touch-icon" href={svgIconDataUri} />
         {/* You can add more specific sizes for apple-touch-icon if you have them, e.g.: */}
         {/* <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" /> */}
         {/* <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" /> */}
         {/* <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" /> */}
-        
         {/* <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" /> */}
         {/* <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" /> */}
       </head>
